19db3839be3033c5c0339223aeb79ea8fae629c8
Pierre de Brosses <p.de-brosses2@exeter.ac.uk>
2023-12-06 12:14:18 +0000

    Added high scores, replicated original functionanlities

--- main.py
+++ main.py
@@ -50,11 +50,18 @@ def player_login():
     print("Baby let the games begin\n")
     return player_name
 
-def game_logic(player_name):
+def game_logic(album_choice, player_name):
     score, question = 0, 0
 
     while True:
-        selected_album = random.choice(albums_data['albums'])
+        if album_choice == "all":
+            selected_album = random.choice(albums_data['albums'])
+        else:
+            selected_album = next((album for album in albums_data['albums'] if album['name'].lower() == album_choice.lower()), None)
+            if not selected_album:
+                print(f"No album found with the name {album_choice}.")
+                break
+
         album_name = selected_album['name']
         song_title = random.choice(selected_album['songs'])
         song_hint = process_song_title(song_title)
@@ -62,42 +69,69 @@ def game_logic(player_name):
         question += 1
         display_question(album_name, song_hint, question)
 
-        attempt = 0
-        while attempt < 2:
-            attempt += 1
+        correct_guess = False
+        for attempt in range(1, 3):  # Two attempts
             if handle_guess(song_title):
+                correct_guess = True
                 print("Correct!")
                 score = update_score(score, True, attempt)
+                print(f"Score: {score}")
                 break
             else:
-                print("Wrong guess. Try again." if attempt == 1 else "No more attempts.")
-        
-        if attempt == 2 and not handle_guess(song_title):
-            print(f"The correct answer was {song_title}.")
-            break
+                if attempt == 1:
+                    print("Wrong guess. Try again.")
+                else:
+                    print(f"No more attempts. The correct answer was: {song_title}")
+                    break
 
-        print(f"Your current score is: {score}")
+        if not correct_guess:
+            break
 
     print(f"Game over. Your final score is {score}.")
     save_score(player_name, score)
     display_top_scores()
 
 def save_score(player_name, score):
-    with open("scores.csv", "a") as file:
-        file.write(f"{player_name},{score}\n")
-    print("Your score has been saved.")
+    scores = []
+
+    try:
+        with open("scores.csv", "r") as file:
+            scores = [line.strip().split(",") for line in file.readlines()]
+    except FileNotFoundError:
+        pass
+
+    existing_score = next((s for s in scores if s[0] == player_name), None)
+    if existing_score and int(existing_score[1]) < score:
+        scores.remove(existing_score)
+        scores.append([player_name, str(score)])
+    elif not existing_score:
+        scores.append([player_name, str(score)])
+
+    with open("scores.csv", "w") as file:
+        for s in scores:
+            file.write(",".join(s) + "\n")
+
+    # Custom message based on score
+    if score < 10:
+        print("Well, you didn't do very well now, did you?")
+    elif score < 50:
+        print("I guess you did okay, well done, your knowledge of Taylor Swift songs isn't too bad.")
+    else:
+        print("Well done, you have a brilliant knowledge of Taylor Swift songs :D")
+
 
 def display_top_scores():
     try:
         with open("scores.csv", "r") as file:
             scores = [line.strip().split(",") for line in file.readlines()]
-            scores.sort(key=lambda x: int(x[1]), reverse=True)  # Sort by score in descending order
+            scores.sort(key=lambda x: int(x[1]), reverse=True)
 
         print("\nTop Scores:")
         for name, score in scores:
             print(f"{name} - {score}")
     except FileNotFoundError:
         print("No scores recorded yet.")
+
 
 def title_intro():
     print(f"{cr.Fore.YELLOW}   _______       __")
@@ -127,12 +161,53 @@ def main_intro():
     print()
     input()
 
+def game_menu(player_name):
+    last_choice = None
+
+    while True:
+        if not last_choice:
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}Love's a game, wanna play?")
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}1. Play with all song titles")
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}2. Play with a specific album")
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}3. View top scores")
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}4. Exit")
+
+            choice = input("Enter your choice (1-4): ")
+        else:
+            choice = last_choice
+
+        if choice == "1":
+            game_logic("all", player_name)
+            last_choice = "1"
+        elif choice == "2":
+            if not last_choice:
+                album_choice = input("Enter the album name: ").title()
+            game_logic(album_choice, player_name)
+            last_choice = "2"
+        elif choice == "3":
+            display_top_scores()
+            last_choice = None
+        elif choice == "4":
+            print(f"{cr.Fore.WHITE}{cr.Style.BRIGHT}Goodbye, goodbye, goodbye, you were bigger than the whole sky")
+            break
+        else:
+            print(f"{cr.Fore.RED}{cr.Style.DIM}Please choose a valid option.")
+            last_choice = None
+
+        play_again = input("Do you want to play another game? (Press Enter for Yes or type 'no'): ").lower()
+        if play_again == "no":
+            last_choice = None
+
+
+
 def start_game():
     print("Welcome to the Taylor Swift Song Guessing Game!")
     title_intro()
     main_intro()
     player_name = player_login()
-    game_logic(player_name)
+    game_menu(player_name)
+
+
 
 if __name__ == "__main__":
     start_game()
